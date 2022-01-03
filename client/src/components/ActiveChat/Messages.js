c29a46a2f70401465e5fff64a06ff6e21100e3a5
“[Peter <pwdrysdale@gmail.com>
2022-01-03 13:43:36 -0500

    Did not commit client, sorry

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,33 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
+  const [sortedMessages, setSortedMessages] = React.useState([]);
+
+  console.log(messages);
+
+  useEffect(() => {
+    if (messages) {
+      const sortedMessages = [...messages].sort((a, b) => {
+        if (a.createdAt.valueOf() < b.createdAt.valueOf()) {
+          return -1;
+        }
+        if (a.createdAt.valueOf() > b.createdAt.valueOf()) {
+          return 1;
+        }
+        return 0;
+      });
+      setSortedMessages(sortedMessages);
+    }
+    console.log(messages);
+  }, [messages]);
 
   return (
     <Box>
-      {messages
-        .sort((a, b) =>
-          new Date(a.updatedAt).valueOf() > new Date(b.updatedAt).valueOf()
-            ? 1
-            : -1
-        )
-        .map((message, idx) => {
-          const time = moment(message.createdAt).format("h:mm");
+      {sortedMessages.map((message, idx) => {
+        const time = moment(message.createdAt).format("h:mm");
 
-          return message.senderId === userId ? (
-            <SenderBubble key={idx} text={message.text} time={time} />
-          ) : (
-            <OtherUserBubble
-              key={idx}
-              text={message.text}
-              time={time}
-              otherUser={otherUser}
-            />
-          );
-        })}
+        return message.senderId === userId ? (
+          <SenderBubble key={idx} text={message.text} time={time} />
+        ) : (
+          <OtherUserBubble
+            key={idx}
+            text={message.text}
+            time={time}
+            otherUser={otherUser}
+          />
+        );
+      })}
     </Box>
   );
 };
