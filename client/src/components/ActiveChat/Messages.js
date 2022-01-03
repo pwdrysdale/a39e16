3e02c955e32309e3d8f60df167188e120b65a3c4
“[Peter <pwdrysdale@gmail.com>
2022-01-03 15:40:26 -0500

    implemented usememo and usecallback in messages as opposed to the use state use effect

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,48 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useMemo } from "react";
 import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
-  const [sortedMessages, setSortedMessages] = React.useState([]);
 
-  console.log(messages);
+  const sortMessages = useCallback((msgs) => {
+    if (!msgs) return [];
+    if (msgs.length === 0) return msgs;
+    return msgs.sort((a, b) => {
+      return a.createdAt.valueOf() - b.createdAt.valueOf() ? 1 : -1;
+    });
+  }, []);
 
-  useEffect(() => {
-    if (messages) {
-      const sortedMessages = [...messages].sort((a, b) => {
-        if (a.createdAt.valueOf() < b.createdAt.valueOf()) {
-          return -1;
-        }
-        if (a.createdAt.valueOf() > b.createdAt.valueOf()) {
-          return 1;
-        }
-        return 0;
-      });
-      setSortedMessages(sortedMessages);
-    }
-    console.log(messages);
-  }, [messages]);
-
-  return (
-    <Box>
-      {sortedMessages.map((message, idx) => {
-        const time = moment(message.createdAt).format("h:mm");
-
-        return message.senderId === userId ? (
-          <SenderBubble key={idx} text={message.text} time={time} />
-        ) : (
-          <OtherUserBubble
-            key={idx}
-            text={message.text}
-            time={time}
-            otherUser={otherUser}
-          />
-        );
-      })}
-    </Box>
+  const sortedMessages = useMemo(
+    () => sortMessages(messages),
+    [messages, sortMessages]
   );
+
+  const renderedMessages = useMemo(() => {
+    return sortedMessages.map((message, idx) => {
+      const time = moment(message.createdAt).format("h:mm");
+
+      return message.senderId === userId ? (
+        <SenderBubble key={idx} text={message.text} time={time} />
+      ) : (
+        <OtherUserBubble
+          key={idx}
+          text={message.text}
+          time={time}
+          otherUser={otherUser}
+        />
+      );
+    });
+  }, [sortedMessages, otherUser, userId]);
+
+  return <Box>{renderedMessages}</Box>;
 };
 
 export default Messages;
